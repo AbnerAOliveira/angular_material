74a735ac77a5b38e59f34eb549e1e4446ce92daa
Abner Alves de Oliveira <abner.oliveira@ibm.com>
2020-05-24 23:22:39 -0300

    Card

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -10,8 +10,7 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatListModule} from '@angular/material/list';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatExpansionModule} from '@angular/material/expansion';
-
-
+import {MatCardModule} from '@angular/material/card';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -25,6 +24,7 @@ const MaterialComponents = [
   MatListModule,
   MatGridListModule,
   MatExpansionModule,
+  MatCardModule,
 ]
 
 
