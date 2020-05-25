63f45a4b547a19cbed838198ac017a6ea6b4a4aa
Abner A Oliveira <abner.oliveira@ibm.com>
2020-05-25 14:05:23 -0300

    Input

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -13,7 +13,8 @@ import {MatExpansionModule} from '@angular/material/expansion';
 import {MatCardModule} from '@angular/material/card';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatStepperModule} from '@angular/material/stepper';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 
 const MaterialComponents = [
@@ -31,6 +32,8 @@ const MaterialComponents = [
   MatCardModule,
   MatTabsModule,
   MatStepperModule,
+  MatFormFieldModule,
+  MatInputModule,
 ]
 
 
