fb6dd99b3a9bc80d8757a444fe09c49f11aaf6e5
Abner A Oliveira <abner.oliveira@ibm.com>
2020-05-25 13:53:13 -0300

    Streppers

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -12,6 +12,8 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatCardModule} from '@angular/material/card';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatStepperModule} from '@angular/material/stepper';
+
 
 
 const MaterialComponents = [
@@ -28,6 +30,7 @@ const MaterialComponents = [
   MatExpansionModule,
   MatCardModule,
   MatTabsModule,
+  MatStepperModule,
 ]
 
 
